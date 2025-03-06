de5ea5f59d1ef10ee6c71277a68c0cf8e6a0ff13
Nanji-Huaji <hua_ji@outlook.com>
2025-03-06 12:42:08 +0000

    创建了dataset类

---
+++ dataset.py
@@ -0,0 +1,21 @@
+import torch
+
+import numpy as np
+import pandas as pd
+
+class SentimentDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path, tokenizer, max_len):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def collate_fn(self, batch):
+        pass
+
+    def get_labels(self):
+        pass
+
