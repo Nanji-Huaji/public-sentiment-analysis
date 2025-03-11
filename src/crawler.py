9b5f37ed501e324375c9ea434a4044aa810d21f5
Nanji-Huaji <hua_ji@outlook.com>
2025-03-11 17:03:51 +0000

    搭建大小模型推理的类，完成demo的编写

---
+++ src/crawler.py
@@ -0,0 +1,10 @@
+"""
+在这里写爬虫代码，你们可以自己工具函数，也可以直接调用第三方库，结果需要保存到data/crawler/crawler_data.csv中
+
+存储格式要求：
+第一行为表头，包括：["text", "target", "platform", "time"]
+    - text: 文本内容
+    - target: 评论对象
+    - platform: 社交媒体
+    - time: 爬取时间
+"""
--- src/inference.py
+++ src/inference.py
@@ -1,15 +1,18 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+from transformers import BertTokenizer
+import openai
 
 
 class StanceDetection:
-    def __init__(self, model, tokenizer):
+    def __init__(self, model="models/output/checkpoint-825", tokenizer=None):
         id2label = {0: "AGAINST", 1: "POSITIVE", 2: "NEITHER"}
         label2id = {"AGAINST": 0, "POSITIVE": 1, "NEITHER": 2}
         model = AutoModelForSequenceClassification.from_pretrained(
             model, num_labels=3, id2label=id2label, label2id=label2id
         )
         self.model = model
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        # tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        tokenizer = BertTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased")
         self.tokenizer = tokenizer
 
     def classify(self, text, target):
@@ -19,3 +22,35 @@ class StanceDetection:
 
     def __call__(self, text, target):
         return self.classify(text, target)
+
+    def process_csv(self, csv_file):
+        pass
+
+
+class LLMInference:
+    def __init__(self, model, api_base, api_key):
+        self.model = model
+        self.api_base = api_base
+        self.api_key = api_key
+        openai = openai.OpenAI(api_key=api_key, api_base=api_base, model=model)
+
+    def inference(self, text):
+        response = openai.Completion.create(engine=self.model, prompt=text)
+        return response.choices[0].text
+
+    def __call__(self, text):
+        return self.inference(text)
+
+
+class SLMInference:
+    def __init__(self, model, api_base, api_key):
+        pass
+
+    def inference(self, text):
+        pass
+
+    def __call__(self, text):
+        return self.inference(text)
+
+    def summary(self, text):
+        pass
